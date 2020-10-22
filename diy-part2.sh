713d693279fd8a835991ca92036b0a75499e62d1
tianku1985 <67080934+tianku1985@users.noreply.github.com>
2020-10-22 10:27:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,12 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.8.5/g' package/base-files/files/bin/config_generate
+
+ 
+#修改
+sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V %C by TianKu'/g" package/base-files/files/etc/openwrt_release
+ sed -i '$a export LANG=zh_CN.UTF-8' package/base-files/files/etc/profile
+#默认密码 root
+ sed -i 's/root:.*/root:$1$tTPCBw1t$ldzfp37h5lSpO9VXk4uUE\/:18336:0:99999:7:::/g' package/base-files/files/etc/shadow
